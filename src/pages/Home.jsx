926d5c99c51c779317110a6c8fe9f0cb32fe6921
jefriwahyu <memberjefry1@gmail.com>
2025-08-12 10:27:37 +0700

    set up tailwind and theme customization

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+    return <div className="min-h-screen">
+
+        {/* Theme Toggle*/}
+
+        {/* Background Effects */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+
+    </div>
+}
