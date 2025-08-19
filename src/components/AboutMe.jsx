d6f50fcf5916d2d0ad0cea2e8593186a9f23d0b6
jefriwahyu <memberjefry1@gmail.com>
2025-08-19 20:37:02 +0700

    update aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -32,7 +32,7 @@ export const AboutMe = () => {
               </a>
 
               <a
-                href="public/resume/CV_JefriWP_ENG.pdf"
+                href="/resume/CV_JefriWP_ENG.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
